0f22be55de8fef3e384325e4137ac7388648b43a
WillDyer <will.dyer9@gmail.com>
2024-09-28 13:50:56 +0100

    maya2024&2025 compatibility

--- export_usd.py
+++ export_usd.py
@@ -3,12 +3,20 @@ import os
 import sys
 
 from maya import OpenMayaUI as omui
-from PySide2.QtCore import *
-from PySide2.QtGui import *
-from PySide2.QtWidgets import QWidget
-from PySide2.QtWidgets import *
-from PySide2.QtUiTools import *
-from shiboken2 import wrapInstance
+try:
+    from PySide6.QtCore import *
+    from PySide6.QtGui import *
+    from PySide6.QtWidgets import QWidget
+    from PySide6.QtWidgets import *
+    from PySide6.QtUiTools import *
+    from shiboken6 import wrapInstance
+except ModuleNotFoundError:
+    from PySide2.QtCore import *
+    from PySide2.QtGui import *
+    from PySide2.QtWidgets import QWidget
+    from PySide2.QtWidgets import *
+    from PySide2.QtUiTools import *
+    from shiboken2 import wrapInstance
 
 import maya.cmds as cmds
 # from playsound import playsound
