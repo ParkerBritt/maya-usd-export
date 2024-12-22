d06573a0c79dc8ab1e681ae0d6e96c714de3d577
parker <parker.g.britt@gmail.com>
2024-12-22 22:19:01 +0000

    test: create maya standalone, supress warnings

--- tests/tests.py
+++ tests/tests.py
@@ -1,4 +1,14 @@
 import pytest
+# from maya_usd_export import interface
+import maya_usd_export
+
+import maya.standalone 
+maya.standalone.initialize()
+
+
 
 def test_foo():
     assert True == True
+
+def pytest_sessionfinish(session, exitstatus):
+    maya.standalone.uninitialize()
