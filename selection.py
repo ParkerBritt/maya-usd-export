da25c0736a12e274ade3b5877e9d86d654598398
Will Dyer <37468008+WillDyer@users.noreply.github.com>
2025-01-02 16:07:53 +0000

    fix: namespaces (#5)
    - remove taking first namespace in list
    - search and find namespace on geo
    - store get_characters in dictionary for unique namespaces

--- selection.py
+++ selection.py
@@ -9,28 +9,29 @@ class Selection():
         self.render_geo_whitelist = render_geo_whitelist
         self.export_rig = export_rig
 
-        self.namespace = self.check_for_namespaces()
+        self.namespaces = self.check_for_namespaces()
 
-        characters, matching_groups = self.get_characters()
+        character_dict = self.get_characters()
 
-        for i, character in enumerate(characters):
-            joint_grp_list = self.select_joint_grps(character)
-            filtered_children, root_prim = self.get_geo_grps(group_name=matching_groups[i])
+        for character in character_dict.values():
+            self.namespace = character["namespace"]
+            joint_grp_list = self.select_joint_grps(character["character_name"])
+            filtered_children, root_prim = self.get_geo_grps(group_name=character["matching_groups"])
 
             temp_dict = {
                 "root_prim": root_prim,
                 "filtered_children": filtered_children,
                 "joint_grp_path": joint_grp_list,
-                "group_name": matching_groups[i],
+                "group_name": character["matching_groups"],
                 "namespace": self.namespace
             }
 
-            self.selection_data[character] = temp_dict
+            self.selection_data[character["character_name"]] = temp_dict
 
     def select_joint_grps(self, character):
         joint_grp_list = []
         if self.export_rig is True:
-            for joint_grp in self.get_joint_grps(f"{character}_rig"):
+            for joint_grp in self.get_joint_grps(character):
                 joint_grp_list.append(joint_grp)
             return joint_grp_list
 
@@ -43,34 +44,46 @@ class Selection():
 
         # formatting for namespaces
         if namespaces:
-            namespace = namespaces[0]
+            return namespaces
         else:
-            namespace = None
-        return namespace
+            return None
 
     def get_characters(self):
+        character_dict = {}
+        groups = []
+        
         # formatting for namespaces and finds groups within rig structure
-        if self.namespace:
-            groups = cmds.ls(f"{self.namespace}:geo*", long=True)
+        if self.namespaces:
+            for namespace in self.namespaces:
+                if cmds.ls(f"{namespace}:geo", long=True):
+                    group = cmds.ls(f"{namespace}:geo", long=True)
+                    groups.append(group[0])
+            if not groups:  # other namespaces can exist in maya scenes this catches that error
+                groups.append(cmds.ls("geo", long=True)[0])
         else:
-            groups = cmds.ls("*geo*", long=True)
 
-        matching_groups = []
-        characters = []
-
+            groups.append(cmds.ls("geo", long=True)[0])
+        
         for grp in groups:
             parent = cmds.listRelatives(grp, parent=True, fullPath=True)
             if parent:
                 parent_name = parent[0]
                 if parent_name.endswith("rig"):
-                    characters.append(parent_name)
-                    matching_groups.append(grp)
+                    namespace = parent_name.rpartition(":")[0]
+                    namespace = namespace.replace("|","")
+                    if not namespace:
+                        namespace = None
+                    character_dict[parent_name] = {
+                        "matching_groups": grp,
+                        "character_name": parent_name,
+                        "namespace": namespace
+                        }
 
         # tell user if _rig cannot be found
-        if not characters and not matching_groups:
+        if not character_dict:
             cmds.warning("No parent group to geo found, make sure parent group of geo has _rig suffix \n")
-
-        return (characters, matching_groups)
+        
+        return character_dict
 
     def get_geo_grps(self, group_name):
         #find root and child groups/prims and filter these through specified groups per to export
