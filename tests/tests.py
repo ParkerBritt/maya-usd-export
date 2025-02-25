20205e41ec94e86042254c6d9ba0630379253edc
Parker <parker@parkerbritt.com>
2025-02-25 15:42:34 +0000

    fix: import error

--- tests/tests.py
+++ tests/tests.py
@@ -1,5 +1,14 @@
-import pytest
+import sys
+import os
+
+# check scripts
+scripts_dir = os.path.join(os.getenv("HOME"), "maya", "scripts")
+if not os.path.exists(scripts_dir):
+    raise Exception (f"Scripts directory does not exist {scripts_dir}")
+sys.path.append(scripts_dir)
+
 # from maya_usd_export import interface
+import pytest
 import maya_usd_export
 from maya_usd_export import selection
 import maya.cmds as cmd
