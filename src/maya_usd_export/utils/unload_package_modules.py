66edf9694f4c35438830be6d0fbf6a75a99fdbd2
Parker <parker@parkerbritt.com>
2025-03-12 16:28:47 +0000

    fix: fix __init__ stale reference unloading

--- src/maya_usd_export/utils/unload_package_modules.py
+++ src/maya_usd_export/utils/unload_package_modules.py
@@ -3,7 +3,7 @@
 
 import os, sys, inspect
 
-def unload_package_modules():
+def unload_package_modules(package_name: str = None):
     '''Unloads all modules from the current package
     
     This is important when developing in maya as packages will not reload until
@@ -12,25 +12,23 @@ def unload_package_modules():
 
     Use this at the start of your script
     '''
-    # find package name
-    package_file_path = os.path.sep.join(__file__.split(os.path.sep)[0:-2])
-    print("unloading packages from:", package_file_path)
+
+    # get package name
+    if package_name is None:
+        package_name = __file__.split(os.path.sep)[-3]
+    print("unloading modules from package:", package_name)
 
     # copy to avoid size changing and thread issue as advised in docs:
     # https://docs.python.org/3/library/sys.html
     for module_name, module in sys.modules.copy().items():
-
-        # get path of the module
-        # skip built in modules that don't have a .__file__ attribute
-        module_file_path = getattr(module, "__file__", None)
-        if not module_file_path:
-            continue
-
         # if the module belongs to the current package then remove from sys.modules
-        if module_file_path.startswith(package_file_path):
+        if module_name.startswith("maya_usd_export"):
+            # get path of the module
+            module_file_path = getattr(module, "__file__", None)
+
             # prevents removing the active script
             if module_file_path == __file__:
                 continue
 
-            print("unloading:", module_file_path)
-            sys.modules.pop(module_name)
+            print("unloading:", module_name)
+            del sys.modules[module_name]
