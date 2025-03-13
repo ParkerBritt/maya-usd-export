590da1f86eef94a89a0d6bfb57b52d64d01faa8c
Parker <parker@parkerbritt.com>
2025-03-13 19:05:06 +0000

    feat: populate tree model from outliner

--- src/maya_usd_export/interface/options/selection_options.py
+++ src/maya_usd_export/interface/options/selection_options.py
@@ -1,3 +1,5 @@
+import maya.api.OpenMaya as om
+
 from maya_usd_export.utils import pyside_importer
 _, qtc, qtw, shiboken = pyside_importer.import_all()
 from PySide import QtGui
@@ -9,11 +11,46 @@ class SelectionTree(qtw.QTreeView):
         self.model = QtGui.QStandardItemModel(self)
         self._root_node = self.model.invisibleRootItem()
 
+        self.populate_model()
         self._tree_view.setModel(self.model)
         self._tree_view.expandAll()
 
+
+    def format_model_item(self, item: QtGui.QStandardItem):
+        item.setCheckable(True)
+        item.setEditable(False)
+
     def populate_model(self):
-        pass
+        dag_iterator = om.MItDag()
+        traversed_items = dict()
+
+        while not dag_iterator.isDone():
+            name = dag_iterator.getPath()
+            path = dag_iterator.fullPathName()
+
+            split_path = path[1:].split("|")
+            print(dag_iterator.fullPathName())
+            print(split_path)
+
+            if(len(split_path) == 1):
+                model_item = QtGui.QStandardItem(str(name))
+                self.format_model_item(model_item)
+                self._root_node.appendRow(model_item)
+                # reset traversed_items to reduce memory usage
+                traversed_items = dict()
+            else:
+                parent_path = "|"+"|".join(split_path[:-1])
+
+                model_item = QtGui.QStandardItem(str(name))
+                self.format_model_item(model_item)
+                traversed_items[parent_path].appendRow(model_item)
+                print("parent", parent_path)
+            traversed_items[path] = model_item
+
+
+
+            dag_iterator.next()
+
 
 class SelectionParameters(qtw.QWidget):
     def __init__(self):
