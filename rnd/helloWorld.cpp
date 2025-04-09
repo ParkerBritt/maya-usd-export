64366d4d49f36fbfc436160d661ab2759cbf3662
Parker <parker@parkerbritt.com>
2025-04-09 18:32:17 +0100

    refactor: remove mayausd

--- rnd/helloWorld.cpp
+++ rnd/helloWorld.cpp
@@ -1,13 +1,12 @@
 #include <maya/MSimple.h>
 #include <maya/MIOStream.h>
 #include <maya/MGlobal.h>
+#include <maya/MSelectionList.h>
+#include <maya/MDagPath.h>
 
 #include <pxr/usd/usd/stage.h>
 #include <pxr/usd/usdGeom/xform.h>
 #include <pxr/usd/usdGeom/cube.h>
-
-#include <mayaUsd/fileio/primWriter.h>
-#include <mayaUsd/fileio/writeJobContext.h>
 
 DeclareSimpleCommand( helloWorld, "Autodesk", "2021" );
 
@@ -20,32 +19,11 @@ MStatus helloWorld::doIt( const MArgList& )
     MSelectionList selectionList;
     MGlobal::getActiveSelectionList(selectionList);
 
-    // cout << "selectionlist\n";
-    // selectionList.getSelectionStrings(array);
-    // for(int i=0; i<array.length(); ++i){
-    //     cout << "item: " << array[i].asChar() << "\n";
-    // }
+    MDagPath dagPath;
+    selectionList.getDagPath(0, dagPath);
 
-    // MDagPath dagPath;
-    // selectionList.getDagPath(0, dagPath);
+    cout << "hello2 path: " << dagPath.fullPathName() << "\n";
 
-    MObject mObject;
-    selectionList.getDependNode(0, mObject);
-
-    // VtDictionary userArgs = UsdMayaJobExportArgs::GetDefaultDictionary();
-    // UsdMayaUtil::MDagPathSet dagPaths;
-    // std::vector<double> timeSamples;
-
-    // UsdMayaJobExportArgs exportArgs = UsdMayaJobExportArgs::CreateFromDictionary(userArgs, dagPaths, timeSamples);
-
-    // UsdMayaWriteJobContext jobCtx();
-
-
-    // UsdMayaPrimWriter primWriter(mObject, SdfPath("/FromPrimWriter"), jobCtx);
-
-    // cout << "hello2 path: " << dagPath.fullPathName() << "\n";
-
-    // UsdStageRefPtr stage = UsdStage::CreateInMemory();
     UsdStageRefPtr stage = UsdStage::CreateNew("/home/s5709975/Downloads/usd_cpp_export_test.usda");
 
     UsdGeomXform xform = UsdGeomXform::Define(stage, SdfPath("/CubeGroup"));
