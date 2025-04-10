dc954dfd4cb326e45d7b45ffac70a90a9d568907
parker <parker.g.britt@gmail.com>
2025-04-10 13:54:33 +0100

    feat: add tests, organize directories

---
+++ tests/system-tests/system-tests.py
@@ -0,0 +1,61 @@
+import sys
+import os
+import pytest
+
+import pytest
+import maya.cmds as cmds
+
+import maya.standalone 
+
+
+
+@pytest.fixture(scope="module")
+def plugin_path():
+    plug_path = os.path.normpath(os.path.join(__file__, "../../../build/helloWorld.so"))
+    plug_exists = os.path.exists(plug_path)
+
+    assert plug_exists, f"Could not find export.so at {plug_path}"
+    return plug_path
+
+@pytest.fixture(scope="module")
+def init_standalone():
+    print("starting standalone")
+    maya.standalone.initialize(name="python")
+    yield
+    print("ending standalone")
+    maya.standalone.uninitialize()
+
+@pytest.fixture(scope="function")
+def new_scene():
+    print("creating new scene")
+    cmds.file(new=True, force=True)
+
+@pytest.fixture(scope="module")
+def load_plug(plugin_path, init_standalone):
+    print("loading plugin")
+    if not cmds.pluginInfo(plugin_path, query=True, loaded=True):
+        try:
+            cmds.loadPlugin(plugin_path)
+        except Exception as e:
+            maya.standalone.uninitialize()
+            pytest.fail(f"Failed to load plugin: {e}\n")
+
+    assert cmds.pluginInfo(plugin_path, query=True, loaded=True)
+
+
+
+def test_foo(load_plug, new_scene):
+    print("test foo")
+    cube1 = cmds.polyCube()
+    cube2 = cmds.polyCube()
+    torus1 = cmds.polyTorus()
+    cmds.select([torus1[0]])
+
+    cmds.helloWorld(f"{os.getenv('HOME')}/Downloads/cpp_export_test.usd")
+
+def test_bar(load_plug, new_scene):
+    print("test bar")
+    cube1 = cmds.polyCube()
+
+
+
