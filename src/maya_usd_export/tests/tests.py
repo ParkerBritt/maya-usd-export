adb01faa0ec1ce4a03faf9ddb04dd7bebbb58b33
Parker <parker@parkerbritt.com>
2025-04-11 15:00:30 +0100

    feat: add old edits

--- src/maya_usd_export/tests/tests.py
+++ src/maya_usd_export/tests/tests.py
@@ -18,27 +18,7 @@ maya.standalone.initialize(name="python")
 
 
 
-def test_selection():
-    # create new file
-    cmd.file(new=True, force=True)
-    cube = cmd.polyCube(name="cube")[0]
-
-    # create rig groups
-    render_grp = cmd.group(empty=True, name="render")
-    geo_grp = cmd.group(empty=True, name="geo")
-    foo_grp = cmd.group(empty=True, name="foo_rig")
-
-    # parent rig heirarcy
-    cmd.parent(cube, render_grp)
-    cmd.parent(render_grp, geo_grp)
-    cmd.parent(geo_grp, foo_grp)
-
-    selection_instance = selection.Selection(render_geo_whitelist=['render'], export_rig=False)
-    assert selection_instance
-    assert selection_instance.return_data() == {'|foo_rig': {'root_prim': 'foo_rig', 'filtered_children': ['render'], 'joint_grp_path': None, 'group_name': '|foo_rig|geo', 'namespace': None}}
-
-    # cmd.file(rename="/home/parker/Downloads/new_scene.ma")
-    # cmd.file(save=True, type="mayaAscii")
+def test_export():
 
 def pytest_sessionfinish(session, exitstatus):
     maya.standalone.uninitialize()
